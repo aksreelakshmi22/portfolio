f86d3b08be1634bafb9fd1e6382675239e27100e
AnnaMathews12 <thesaltshibu369@gmail.com>
2025-06-21 10:55:13 +0530

    first push

---
+++ index.js
@@ -0,0 +1,6 @@
+var a=document.getElementById("a")
+var b=document.getElementById("b")
+function call(exp){
+    var ans= eval(a.value + exp + b.value)
+    document.getElementById("Result").innerHTML=ans
+}
